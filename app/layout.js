46ade2b3cb9698faa30da074d6cff72c0c5c73f2
olivia <olivia@olivias-MacBook-Pro.local>
2026-01-30 23:44:23 +0900

    feat: add MS Clarity script

--- app/layout.js
+++ app/layout.js
@@ -70,6 +70,19 @@ export default function RootLayout({ children }) {
                     }}
                 />
 
+                {/* MS Clarity Script */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(c,l,a,r,i,t,y){
+                                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                            })(window, document, "clarity", "script", "v9l5gxis9u");
+                        `
+                    }}
+                />
+
                 {/* 구조화된 데이터 */}
                 <script
                     type="application/ld+json"
